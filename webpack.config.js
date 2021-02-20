167ccb80d9935d82a0bf0f17a71e3dafc94ff2fd
VeroFlores <veritocalla@gmail.com>
2021-02-20 18:38:56 -0500

    generate manifest for PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+/* eslint-disable prefer-regex-literals */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
 
 module.exports = {
 
@@ -9,6 +13,47 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifest({
+      name: 'Petgram-tu app de fotos ',
+      short_name: 'Petgram ',
+      description: 'MCon Petgram puedes encontrar las fotos de animales domésticos',
+      background_color: '#fff',
+      theme_color: '#2196f3',
+      crossorigin: 'use-credentials', // can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        },
+        {
+          src: path.resolve('src/assets/icon.png'),
+          size: '1024x1024' // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve('src/assets/icon.png'),
+          size: '1024x1024',
+          purpose: 'maskable'
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-api-vero.veroflores.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
